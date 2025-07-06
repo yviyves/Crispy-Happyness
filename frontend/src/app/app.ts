0a5312d287019d9a299cf4ad987b661a9a240101
Yves Bürger <daniel-yves.buerger@zollsoft.de>
2025-07-06 14:50:05 +0200

    correctly display login page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,7 +5,12 @@ import {
   MatSidenavModule,
 } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterModule,
+  RouterOutlet,
+  NavigationEnd,
+} from '@angular/router';
 import { gsap } from 'gsap';
 import { Header } from './layout/header/header';
 import { SideNavIsExtendedStore } from './layout/services/side-nav-is-extended-store';
@@ -13,6 +18,8 @@ import { SideNavigation } from './layout/side-navigation/side-navigation';
 import { fadeInOut } from './shared/animations/fade.animation';
 import { Overlay } from './shared/components/overlay/overlay';
 import { OverlayService } from './shared/components/overlay/overlay-service';
+import { filter, take } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   imports: [
@@ -39,8 +46,18 @@ export class App {
   protected sideNavIsExtendedStore = inject(SideNavIsExtendedStore);
   protected overlayService = inject(OverlayService);
   protected router = inject(Router);
+  protected isInitialized = false;
 
   constructor() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        take(1)
+      )
+      .subscribe(() => {
+        this.isInitialized = true;
+      });
+
     effect(() => {
       if (this.overlayService.showOverlay()) {
         gsap.to('#content', {
@@ -57,6 +74,7 @@ export class App {
   }
 
   isLoginPage(): boolean {
+    if (!this.isInitialized) return true;
     return this.router.url === '/login';
   }
 }
