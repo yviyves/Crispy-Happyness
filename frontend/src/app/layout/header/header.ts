6d2c2ddff55b2b76a87602054261dd363c7a6545
Yves Bürger <daniel-yves.buerger@zollsoft.de>
2025-07-06 12:36:51 +0200

    animations for side-navigation

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, effect, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { SideNavIsExtendedStore } from '../services/side-nav-is-extended-store';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
+import { gsap } from 'gsap';
+import { SideNavIsExtendedStore } from '../services/side-nav-is-extended-store';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,24 @@ import { Router } from '@angular/router';
 export class Header {
   sideNavIsExtendedStore = inject(SideNavIsExtendedStore);
   router = inject(Router);
+
+  constructor() {
+    effect(() => {
+      if (this.sideNavIsExtendedStore.isExtended()) {
+        gsap.to('#menu-icon', {
+          rotate: 0,
+          duration: 0.25,
+          ease: 'power2.inOut',
+        });
+      } else {
+        gsap.to('#menu-icon', {
+          rotate: 90,
+          duration: 0.25,
+          ease: 'power2.inOut',
+        });
+      }
+    });
+  }
 
   toggleSideNav() {
     this.sideNavIsExtendedStore.isExtended.update((value) => !value);
